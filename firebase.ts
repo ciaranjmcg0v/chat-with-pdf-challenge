81f32eb6050ec295f86f5b1b7f6db753cfe7e632
Ciaran McGovern <ciaranjmcgovern@gmail.com>
2024-07-26 11:47:05 +0100

    Updated .env with firebase config

--- firebase.ts
+++ firebase.ts
@@ -3,22 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBehuAFXU5mnn-pftrJNCXr5SDIjmxbiSQ",
-  authDomain: "chat-with-pdf-challenge-1cb46.firebaseapp.com",
-  projectId: "chat-with-pdf-challenge-1cb46",
-  storageBucket: "chat-with-pdf-challenge-1cb46.appspot.com",
-  messagingSenderId: "491951572918",
-  appId: "1:491951572918:web:c185f30147f7de647da317",
-  measurementId: "G-5L1FZ2JDD4",
-  cors: [
-    {
-      origin: ["*"],
-      method: ["GET"],
-      maxAgeSeconds: 3600,
-    },
-  ],
-};
+const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG as string);
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
